9adb273638a4603fda164b8d73e75a6ca0c946c3
Tony <tonyqing2022@gmail.com>
2023-08-09 01:56:20 -0500

    initial gql setup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,86 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use actix_web::{guard, web, web::Data, App, HttpRequest, HttpResponse, HttpServer, Result};
+use async_graphql::{http::GraphiQLSource, Schema, Object, EmptyMutation, EmptySubscription, Result as GQLResult, futures_util::lock::Mutex};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse, GraphQLSubscription};
+use mongodb::Database;
+
+pub type Storage = Arc<Mutex<Resolver>>;
+
+struct Resolver {
+    db: Database
+}
+struct Query;
+
+#[Object]
+impl Query {
+    /// Returns the sum of a and b
+    async fn add(&self, a: i32, b: i32) -> i32 {
+
+        let res = a + b;
+        println!("{}", res);
+        res
+    }
+}
+
+struct Mutation;
+
+#[Object]
+impl Mutation {
+    async fn signup(&self, username: String, password: String) -> bool {
+        true
+    }
+    async fn login(&self, username: String, password: String) -> bool{
+       true
+    }
+}
+
+type AppSchema = Schema<Query, Mutation, EmptySubscription>;
+
+async fn index(schema: web::Data<AppSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_graphiql() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(
+            GraphiQLSource::build()
+                .endpoint("/")
+                .subscription_endpoint("/")
+                .finish(),
+        ))
+}
+
+async fn index_ws(
+    schema: web::Data<AppSchema>,
+    req: HttpRequest,
+    payload: web::Payload,
+) -> Result<HttpResponse> {
+    GraphQLSubscription::new(Schema::clone(&*schema)).start(&req, payload)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data(Storage::default)
+        .finish();
+
+    println!("GraphiQL IDE: http://localhost:8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(schema.clone()))
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(
+                web::resource("/")
+                    .guard(guard::Get())
+                    .guard(guard::Header("upgrade", "websocket"))
+                    .to(index_ws),
+            )
+            .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
