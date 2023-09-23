f3750aa8ddd24798ba16c7189c0ba7336daf9c6a
Tony Qing <tonyqing2022@gmail.com>
2023-09-23 11:28:40 -0500

    Added Selection logic

--- website/src/lib/Calendar.svelte
+++ website/src/lib/Calendar.svelte
@@ -1,7 +1,6 @@
-<script>
-	let selectedMonth = '0';
+<script lang="ts">
 	const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	const monthsOfYear = [
+	const monthNames = [
 		'January',
 		'February',
 		'March',
@@ -15,15 +14,10 @@
 		'November',
 		'December'
 	];
-	const monthsAndDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	const numDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-	let date = new Date();
-
-	let focusedDay = date.getDate();
-	let focusedMonth = date.getMonth();
-	let focusedYear = date.getFullYear();
-
-	function getFirstDayOfWeek(month, day, year) {
+	// Zeller's Congruence formula to get the first day of month
+	function getFirstDayOfWeek(month: number, day: number, year: number) {
 		if (month < 3) {
 			month += 12;
 			year -= 1;
@@ -32,7 +26,6 @@
 		const K = year % 100;
 		const J = Math.floor(year / 100);
 
-		// Zeller's Congruence formula
 		const dayOfWeek =
 			(day +
 				Math.floor((13 * (month + 1)) / 5) +
@@ -41,17 +34,73 @@
 				Math.floor(J / 4) -
 				2 * J) %
 			7;
-
-		// Convert the result to a day of the week
-
+		// + 6 offsets the result to match our daysOfWeek array
 		return (dayOfWeek + 6) % 7;
 	}
-	$: currDays = monthsAndDays[focusedMonth];
+
+	let date = new Date();
+
+	// The current page on the calendar
+	let focusedDay = date.getDate();
+	let focusedMonth = date.getMonth();
+	let focusedYear = date.getFullYear();
+	let isDragging = false;
+	export let selectedDate = new Date(
+		Date.parse(`${focusedYear},${focusedMonth + 1},${focusedDay}`)
+	);
+	export let secondSelectedDate: Date | null = null;
+	$: isActive = (day: number) => {
+		let selectedYear = selectedDate.getFullYear();
+		let selectedMonth = selectedDate.getMonth();
+		let selectedDay = selectedDate.getDate();
+		if (secondSelectedDate) {
+			let secondSelectedYear = secondSelectedDate.getFullYear();
+			let secondSelectedMonth = secondSelectedDate.getMonth();
+			let secondSelectedDay = secondSelectedDate.getDate();
+
+			if (
+				selectedMonth == focusedMonth &&
+				selectedYear == focusedYear &&
+				secondSelectedMonth == focusedMonth &&
+				secondSelectedYear == focusedYear
+			) {
+				let predicate = selectedDay <= day + 1 && secondSelectedDay >= day + 1;
+				return predicate;
+			}
+
+			if (selectedYear <= focusedYear && secondSelectedYear >= focusedYear) {
+				const monthOffset = (secondSelectedYear - selectedYear) * 12;
+				const focusedMonthOffset = (focusedYear - selectedYear) * 12;
+
+				if (
+					selectedMonth < focusedMonth + focusedMonthOffset &&
+					secondSelectedMonth + monthOffset > focusedMonth + focusedMonthOffset
+				) {
+					return true;
+				}
+
+				if (selectedMonth === focusedMonth + focusedMonthOffset) {
+					return selectedDay <= day + 1;
+				}
+				if (secondSelectedMonth === focusedMonth) {
+					return secondSelectedDay >= day + 1;
+				}
+			}
+		} else {
+			return (
+				selectedYear === focusedYear && selectedMonth === focusedMonth && selectedDay === day + 1
+			);
+		}
+
+		return false;
+	};
+	$: currDays = numDaysInMonth[focusedMonth];
 	$: startOffset = getFirstDayOfWeek(focusedMonth + 1, 1, focusedYear);
 	$: endOffset = 7 - ((startOffset + currDays) % 7);
 </script>
 
 <section>
+	<h1>{monthNames[focusedMonth] + ' ' + focusedYear}</h1>
 	<button
 		on:click={() => {
 			focusedYear--;
@@ -69,6 +118,13 @@
 	>
 	<button
 		on:click={() => {
+			focusedDay = date.getDate();
+			focusedMonth = date.getMonth();
+			focusedYear = date.getFullYear();
+		}}>Today</button
+	>
+	<button
+		on:click={() => {
 			if (focusedMonth > 10) {
 				focusedMonth = 0;
 				focusedYear++;
@@ -82,19 +138,53 @@
 			focusedYear++;
 		}}>{`>>`}</button
 	>
-	<div>{monthsOfYear[focusedMonth] + ' ' + focusedYear}</div>
 	<header>
 		{#each daysOfWeek as dayName}
-			<h1>{dayName}</h1>
+			<h3>{dayName}</h3>
 		{/each}
 		{#each Array(startOffset).fill(0) as _, day}
-			<div>{monthsAndDays[focusedMonth ? focusedMonth - 1 : 11] - startOffset + day + 1}</div>
+			<div class="outside month">
+				{numDaysInMonth[focusedMonth ? focusedMonth - 1 : 11] - startOffset + day + 1}
+			</div>
 		{/each}
 		{#each Array(currDays).fill(0) as _, day}
-			<div>{day + 1}</div>
+			<div
+				role="cell"
+				tabindex="0"
+				on:mousedown={() => {
+					isDragging = true;
+					const clickedDate = new Date(Date.parse(`${focusedYear},${focusedMonth + 1},${day + 1}`));
+					if (!secondSelectedDate && clickedDate > selectedDate) {
+						secondSelectedDate = clickedDate;
+					} else {
+						secondSelectedDate = null;
+						selectedDate = clickedDate;
+					}
+				}}
+				on:keydown={() => {}}
+				on:mouseup={() => {
+					isDragging = false;
+				}}
+				on:mouseenter={() => {
+					if (isDragging) {
+						const clickedDate = new Date(
+							Date.parse(`${focusedYear},${focusedMonth + 1},${day + 1}`)
+						);
+						if (clickedDate > selectedDate) {
+							secondSelectedDate = clickedDate;
+						} else {
+							secondSelectedDate = null;
+							selectedDate = clickedDate;
+						}
+					}
+				}}
+				class="{isActive(day) ? 'active' : 'inside'} month"
+			>
+				{day + 1}
+			</div>
 		{/each}
-		{#each Array(endOffset).fill(0) as _, day}
-			<div>{day + 1}</div>
+		{#each Array(currDays + startOffset > 34 ? endOffset : endOffset + 7).fill(0) as _, day}
+			<div class="outside month">{day + 1}</div>
 		{/each}
 	</header>
 </section>
@@ -102,13 +192,40 @@
 <style>
 	section {
 		background-color: white;
+		width: 100%;
+		height: 100%;
+		flex: 1;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+		min-width: 700px;
+		user-select: none;
 	}
 	header {
 		display: grid;
 		grid-template-columns: repeat(7, 1fr);
 		text-align: center;
+		min-width: 70px;
 	}
-	header > h1 {
-		border: 1px solid black;
+
+	.month {
+		aspect-ratio: 1;
+		text-align: start;
+		padding: 10px;
+		border: 1px solid rgb(138, 97, 97);
+	}
+	.active {
+		background-color: rgb(112, 137, 173);
+	}
+	.inside:hover {
+		opacity: 0.6;
+	}
+	.inside {
+		background-color: antiquewhite;
+	}
+
+	.outside {
+		background-color: whitesmoke;
+		opacity: 0.4;
 	}
 </style>
--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,14 +1,26 @@
-<script>
+<script lang="ts">
     import Calendar from "$lib/Calendar.svelte";
+    let secondSelectedDate:Date;
+    let selectedDate:Date;
+    $: console.log(selectedDate, secondSelectedDate)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Calendar/>
+<h3>Embarkment</h3>
+<p>{selectedDate}</p>
+<h3>Disembarkment</h3>
+<p>{secondSelectedDate}</p>
+<div>
+    <Calendar bind:selectedDate={selectedDate} bind:secondSelectedDate={secondSelectedDate}/>
+</div>
 
 
 <style>
     :root {
-        background-color: black;
+        display: flex;
+        justify-content: center;
+    }
+    div {
     }
 </style>
