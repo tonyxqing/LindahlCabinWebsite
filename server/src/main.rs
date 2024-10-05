e0bf9826b7c7be200883b23d0570cb10f26d0806
tonyxqing <tonyqing2022@gmail.com>
2024-10-05 08:29:03 -0500

    adding cargo deps to action and updated rustls invocation

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::BufReader;
 use actix_cors::Cors;
 use actix_web::{
     get, guard,
@@ -83,6 +85,29 @@ async fn main() -> std::io::Result<()> {
         .data(ar.clone())
         .finish();
 
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .unwrap();
+
+    let mut certs_file = BufReader::new(File::open("cert.pem").unwrap());
+    let mut key_file = BufReader::new(File::open("key.pem").unwrap());
+
+    // load TLS certs and key
+    // to create a self-signed temporary cert for testing:
+    // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
+    let tls_certs = rustls_pemfile::certs(&mut certs_file)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    let tls_key = rustls_pemfile::pkcs8_private_keys(&mut key_file)
+        .next()
+        .unwrap()
+        .unwrap();
+
+    // set up TLS config options
+    let tls_config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(tls_certs, rustls::pki_types::PrivateKeyDer::Pkcs8(tls_key))
+        .unwrap();
     HttpServer::new(move || {
         App::new()
             .wrap(Cors::permissive())
@@ -98,7 +123,7 @@ async fn main() -> std::io::Result<()> {
             .service(greet)
             .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
     })
-    .bind_rustls_0_23("0.0.0.0:8000")?
+    .bind_rustls_0_23("0.0.0.0:8000", tls_config)?
     .run()
     .await
 }
